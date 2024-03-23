8c04095fe4a76439712bd8a3a028210ccaf196ec
Yero2011 <163444848+Yero2011@users.noreply.github.com>
2024-03-23 20:33:28 +0400

    Add files via upload

---
+++ python.bat
@@ -0,0 +1,16 @@
+@echo off
+title python
+color a
+echo install python
+pause
+echo installed 1%
+pause
+echo instaled 47%
+pause 
+echo instaled 99%
+pause
+echo Your pc restarting now
+pause
+color 4
+echo Hacking
+pause
